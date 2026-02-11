0e7f3febf6df4a88115b1967957ffbe002fefc75
boss9292 <linminggeng19@gmail.com>
2026-02-11 16:04:52 -0600

    real

--- lib/env.ts
+++ lib/env.ts
@@ -1,3 +1,3 @@
-export const API_BASE = (
-  process.env.NEXT_PUBLIC_API_URL ?? "http://127.0.0.1:8000"
-).replace(/\/+$/, "");
+export const API_BASE =
+  process.env.NEXT_PUBLIC_API_URL ??
+  "https://studybuddy-backend-production.up.railway.app";
